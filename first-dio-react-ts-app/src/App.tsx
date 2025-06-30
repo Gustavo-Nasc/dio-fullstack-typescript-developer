db5188501dbcf8fdd166a345d16f0d68fb7a99ea
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2025-06-30 12:10:37 -0300

    feat: working with components inside components in react

--- first-dio-react-ts-app/src/App.tsx
+++ first-dio-react-ts-app/src/App.tsx
@@ -1,15 +1,14 @@
 import { Card } from './components/Card'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
+import { Layout } from './Layout'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Card title="First Card" text="This is the first card" details="Here are the details for the first card" />
-      <Card title="Second Card" text="I'm the second card" details="There are some details from the second card" />
-      <Footer />
+      <Layout>
+          <Card title="First Card" text="This is the first card" details="Here are the details for the first card" />
+          <Card title="Second Card" text="I'm the second card" details="There are some details from the second card" />
+      </Layout>
     </>
   )
 }
